8bf4eef264e4239d7de73e958d0963531d1748c9
hootie <me@urielsanchez.com>
2026-01-15 23:13:18 -0600

    ci: deploy to elastic beanstalk

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const http = require('http');
-require('dotenv').config();
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const pool = require('./config/db');
-const initSockets = require('./sockets');
-const routes = require('./routes');
-const NodeCache = require('node-cache');
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+require("dotenv").config();
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const pool = require("./config/db");
+const initSockets = require("./sockets");
+const routes = require("./routes");
+const NodeCache = require("node-cache");
 
 const cache = new NodeCache({ stdTTL: 120 }); // TTL de 120 segundos (2 minutos)
 const app = express();
@@ -18,18 +18,20 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(__dirname));
 
-app.use('/', routes);
+app.use("/", routes);
 
-app.get('/health', (req, res) => {
-  res.json({ status: 'OK', message: 'Servidor funcionando' });
+app.get("/health", (req, res) => {
+  res.json({ status: "OK", message: "Servidor funcionando" });
 });
 
 async function start() {
   const server = http.createServer(app);
   const io = initSockets(server, pool);
 
-  app.set('io', io);
-  server.listen(PORT, () => console.log(`🚀 Servidor corriendo en puerto ${PORT}`));
+  app.set("io", io);
+  server.listen(PORT, () =>
+    console.log(`🚀 Servidor corriendo en puerto ${PORT}`)
+  );
 }
 
 start();
